f15fd734f2c2818ae2ea72276c881a081c74eebf
fhranzM <126442131+fhranzM@users.noreply.github.com>
2023-02-27 13:58:09 +0800

    POMODORO APP

---
+++ GETREDYApp/app/src/main/java/com/example/getred_yapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.getred_yapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
